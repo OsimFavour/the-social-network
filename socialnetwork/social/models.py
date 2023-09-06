27a6cb6c5ccd22e7fda71644dad6240abad1b350
OsimFavour <famos204@gmail.com>
2023-09-06 11:36:25 +0100

    Update post lists and profile

--- socialnetwork/social/models.py
+++ socialnetwork/social/models.py
@@ -28,7 +28,7 @@ class UserProfile(models.Model):
     birth_date = models.DateField(null=True, blank=True)
     location = models.CharField(max_length=100, blank=True, null=True)
     picture = models.ImageField(upload_to='uploads/profile_pictures', default='uploads/profile_pictures/default.png')
-    
+    followers = models.ManyToManyField(User, blank=True, related_name='followers')
 
 # All we need to use django signals
 # sender -> User
